6f3d659f7450faadf55c63a29612622243a94c32
Jeni <jenifer969696@gmail.com>
2025-12-18 20:04:28 -0300

    ej 1

---
+++ tp-mysql-cruz.sql
@@ -0,0 +1,10 @@
+-------------------------------------------------
+--Ejercicio 1 – Crear Base de Datos
+--Crear una base de datos llamada veterinaria_patitas_felices.
+
+DROP DATABASE IF EXISTS veterinaria_patitas_felices;
+CREATE DATABASE veterinaria_patitas_felices;
+USE veterinaria_patitas_felices;
+
+-------------------------------------------------
+--Ejercicio 2 – Crear tabla duenos
